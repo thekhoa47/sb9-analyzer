f8b76b1450f581c78eac806b3fb318d600f29697
Kyle Nguyen <ng.thekhoa@gmail.com>
2025-08-22 16:14:55 -0700

    🐛 CORS Error

--- backend/app/main.py
+++ backend/app/main.py
@@ -57,7 +57,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://0.0.0.0:3000",
-        "https://sb9-analyzer.onrender.com",
+        "https://sb9-analyzer.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
