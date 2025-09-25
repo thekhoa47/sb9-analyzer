34ce92a2b03be037ac69e34a6c19f10ad9cc0090
Kyle Nguyen <ng.thekhoa@gmail.com>
2025-09-24 23:34:02 -0700

    🐛 Final dockerfile, cloudbuild and entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -eu
 
-# Expand single-secret .env into real env vars
-if [ "${DOTENV:-}" != "" ]; then
-  echo "$DOTENV" > /tmp/runtime.env
-  set -a; . /tmp/runtime.env; set +a
-fi
+set -a; . /tmp/runtime.env; set +a
 
 exec "$@"
